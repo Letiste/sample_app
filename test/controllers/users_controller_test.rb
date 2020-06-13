932bd5161138f714d34d344332ee8f2f2ddad66c
Léo Salé <leo.sale72@gmail.com>
2020-06-13 17:10:23 +0200

    Finish layout and routes

---
+++ test/controllers/users_controller_test.rb
@@ -0,0 +1,10 @@
+require 'test_helper'
+
+class UsersControllerTest < ActionDispatch::IntegrationTest
+  test "should get new" do
+    get signup_path
+    assert_response :success
+    assert_select "title", full_title("Sign Up")
+  end
+
+end
