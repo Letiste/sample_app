015028746a8a439f150cea6ec798bd5f71985d53
Léo Salé <leo.sale72@gmail.com>
2020-07-08 19:11:04 +0200

    Implement basic login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,3 @@
 class ApplicationController < ActionController::Base
-
-  def hello
-    render html: "<h1> hello, world! <h1>"
-  end
+  include SessionsHelper
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -12,4 +12,9 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true
 
   has_secure_password
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
 end
